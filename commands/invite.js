f378086168c093ec754e3ba75eff8fa4a59bf4be
Glitch (lmsbot) <none>
2021-04-19 12:14:04 +0000

    Updated invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'invite',
     description: "Sends the invite link of Poison Lab",
     execute(message, args){
-        message.channel.send('https://discord.gg/6G4bSMzaw4');
+        message.channel.send('https://discord.gg/ZCvxvH6rhE');
     }
 }
