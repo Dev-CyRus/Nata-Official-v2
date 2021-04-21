e140de8bee9f987bc914b30e99b21f39eeba86af
Glitch (lmsbot) <none>
2021-04-21 07:39:06 +0000

    Fixed delete

---
+++ commands/fb.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'fb',
+    description: "Sends the link of revive style community group",
+    execute(message, args){
+        message.channel.send('https://www.facebook.com/112841460459122');
+    }
+}
